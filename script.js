2df0a9241712cac3d63c6e43280e02acda049a72
Abraham Reto <120141978+vampiro832@users.noreply.github.com>
2023-04-25 18:26:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var generateBtn = document.getElementById("generate");
+var writePassword = document.getElementById("password");
+
+generateBtn.addEventListener("click", function () {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      password = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  writePassword.value = password;
+});
+
